796800ce95763be56090e09ee264e58fda964074
Vivyanne-504 <66225228+Vivyanne-504@users.noreply.github.com>
2020-08-19 23:14:59 +0100

    simple image slider

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+const container = document.querySelector('.images');
+
+
+let counter = 0;
+
+nextBtn.addEventListener('click',nextSlide);
+prevBtn.addEventListener('click',prevSlide);
+
+function nextSlide(){
+  counter++;
+  container.style.backgroundImage = 'url(img/bcg-${counter}.jpg';
+
+
+}
